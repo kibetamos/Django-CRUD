4d248585dea29e8caea6b9d1ac2f029d6d4a314c
kibet kilel <amos.rotich@strathmore.edu>
2022-06-24 08:21:18 +0300

    Create views

--- I4G017970JAF/blog/views.py
+++ I4G017970JAF/blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
 # Create your views here.
+# from django.views.generic.edit import (
+#     CreateView,
+#     UpdateView,
+#     DeleteView )# new
+from django.urls import reverse_lazy # new
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog: all')
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog: all')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog: all')
